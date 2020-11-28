c9e7c83fa171abf7239bd69809242b4dacde76d5
Jiwon Kim <jiwonkimpark@naver.com>
2020-11-28 22:26:53 +0900

    fixed error

--- backend/src/main/java/org/haeboja/backend/dao/reservation/JdbcReservationRepository.java
+++ backend/src/main/java/org/haeboja/backend/dao/reservation/JdbcReservationRepository.java
@@ -1,20 +1,24 @@
 package org.haeboja.backend.dao.reservation;
 
+import org.haeboja.backend.dto.Reservation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;`
+import java.util.List;
 
 @Repository
-public class JdbcReservationRepository {
+public class JdbcReservationRepository implements ReservationRepository{
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    @Override
+
     public List<Reservation> getReservationCountByHouseIdAndRoomId(long houseId, long roomId){
-        return jdbcTemplate.query(
-                "select count(*) from reservation where houseId = ? and roomId = ?"
-        );
+//        return jdbcTemplate.query(
+//                "select count(*) from reservation where houseId = ? and roomId = ?"
+//        );
+        return null;
     }
+
 }
